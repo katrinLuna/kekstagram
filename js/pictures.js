40ad4bdeff3c96abc5fe07ac944926cf3566258e
katrinLuna <kate.nik2017@yandex.ru>
2018-06-21 20:39:13 +0300

    #3 refactoring createNewPhoto function

--- js/pictures.js
+++ js/pictures.js
@@ -2,15 +2,12 @@
 var COUNT_ELEMENT = 25;
 var USER_COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var PHOTO_DESCRIPTION = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
-
 var usersPhotos = [];
 
-//functions
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-//loop for creative new user photo element
 var initPhotos = function () {
   for (var i = 0; i < COUNT_ELEMENT; i++) {
     var newPhotoBuild = {
@@ -19,7 +16,6 @@ var initPhotos = function () {
       comments: USER_COMMENTS[getRandomNumber(0, USER_COMMENTS.length-1)],
       description: PHOTO_DESCRIPTION[getRandomNumber(0, PHOTO_DESCRIPTION.length-1)]
     };
-
     usersPhotos.push(newPhotoBuild);
   };
 
@@ -29,12 +25,12 @@ var initPhotos = function () {
 var similarListElement = document.querySelector('.pictures');
 var similarPhotoTemplates = document.querySelector('#picture').content.querySelector('.picture__link');
 
-var createNewPhoto = function () {
+var createNewPhoto = function (photo) {
   var photoElement = similarPhotoTemplates.cloneNode(true);
 
-  photoElement.querySelector('.picture__img').src = usersPhotos.url;
-  /*photoElement.querySelector('picture__stat--likes').textContent = usersPhotos.likes.toString(10);
-  photoElement.querySelector('picture__stat--comments').textContent = usersPhotos.comments;*/
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__stat--likes').textContent = photo.likes.toString(10);
+  photoElement.querySelector('.picture__stat--comments').textContent = photo.comments;
 
   return photoElement;
 }
