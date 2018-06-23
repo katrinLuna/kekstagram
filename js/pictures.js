5f36c382089c5af87c31233e1ed409bff5d9b550
katrinLuna <kate.nik2017@yandex.ru>
2018-06-23 20:12:41 +0300

    #3 almost complete, just left new comments

--- js/pictures.js
+++ js/pictures.js
@@ -5,17 +5,36 @@ var PHOTO_DESCRIPTIONS = ['Тестим новую камеру!', 'Затуси
 var usersPhotos = [];
 var similarListElement = document.querySelector('.pictures');
 var similarPhotoTemplateElement = document.querySelector('#picture').content.querySelector('.picture__link');
+var bigPhotoElement = document.querySelector('.big-picture');
+var socialCommentElement = document.querySelector('.social__comment');
+var socialCommentListElement = document.querySelector('.social__comments');
 
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+var hideElement = function (elem) {
+  elem.classList.add('visually-hidden');
+};
+
+var getRandomComments = function () {
+  var commentsGenerate = [];
+  var commentCount = getRandomNumber(1, 2);
+
+  for (var i = 0; i < commentCount; i++) {
+    commentsGenerate.push(USER_COMMENTS[getRandomNumber(0, USER_COMMENTS.length - 1)]);
+  }
+
+  return commentsGenerate;
+};
+
 
 var initPhotos = function () {
   for (var i = 0; i < COUNT_ELEMENT; i++) {
     var newPhotoBuild = {
       url: 'photos/' + (i + 1) + '.jpg',
       likes: getRandomNumber(15, 200),
-      comments: USER_COMMENTS[getRandomNumber(0, USER_COMMENTS.length - 1)],
+      comments: getRandomComments(),
       description: PHOTO_DESCRIPTIONS[getRandomNumber(0, PHOTO_DESCRIPTIONS.length - 1)]
     };
     usersPhotos.push(newPhotoBuild);
@@ -29,11 +48,12 @@ var createNewPhoto = function (photo) {
   var photoElement = similarPhotoTemplateElement.cloneNode(true);
 
   photoElement.querySelector('.picture__img').src = photo.url;
-  photoElement.querySelector('.picture__stat--likes').textContent = photo.likes.toString(10);
-  photoElement.querySelector('.picture__stat--comments').textContent = photo.comments;
+  photoElement.querySelector('.picture__stat--likes').textContent = photo.likes;
+  photoElement.querySelector('.picture__stat--comments').textContent = photo.comments.length;
 
   return photoElement;
 };
+
 
 var renderPhotos = function (photos) {
   var fragment = document.createDocumentFragment();
@@ -48,8 +68,31 @@ var renderPhotos = function (photos) {
 var init = function () {
   usersPhotos = initPhotos();
   renderPhotos(usersPhotos);
-  /* showBigPhoto(userPhotos[0]);*/
+  /* recreateBigPhoto(usersPhotos[0]);*/
 };
 
 init();
 
+var createSocialComment = function () {
+  var fragment = document.createDocumentFragment();
+  var comment = socialCommentElement.cloneNode(true);
+
+
+
+  socialCommentListElement.appendChild(fragment);
+}
+
+var createBigPhoto = function (photo) {
+  bigPhotoElement.classList.remove('hidden');
+  bigPhotoElement.querySelector('.big-picture__img img').src = photo.url;
+  bigPhotoElement.querySelector('.likes-count').textContent = photo.likes;
+  bigPhotoElement.querySelector('.comments-count').textContent = photo.comments.length;
+  bigPhotoElement.querySelector('.social__caption').textContent = photo.description;
+
+  return bigPhotoElement;
+};
+
+hideElement(document.querySelector('.social__comment-count'));
+hideElement(document.querySelector('.social__loadmore'));
+
+createBigPhoto(usersPhotos[0]);
