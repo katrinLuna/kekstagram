1971747416120e3fe891016baa775159dcc2febb
katrinLuna <kate.nik2017@yandex.ru>
2018-07-10 13:44:48 +0300

    #6-1 think i'm realize how callback will work

--- js/backend.js
+++ js/backend.js
@@ -2,15 +2,31 @@
 
 (function (){
   var DOWNLOAD_URL = 'https://js.dump.academy/kekstagram/data';
+  var SERVER_ANSWER_OK = 200;
 
-  var xhr = new XMLHttpRequest;
-  xhr.responseType = 'json';
-  xhr.open('GET', DOWNLOAD_URL);
+  /*только это выносится в файл в котором будет использоваться результат работы функции
+  и будет вызываться вот так window.backend.download(onSuccess);
 
-  xhr.addEventListener('load', function () {
-    console.log(xhr.response);
-  })
+  var onSuccess = function (data) {//поэтому без window объявляем
+    console.log(data);
+  }*/
 
-  xhr.send();
+  window.backend = {
+    download: function (onLoad) {
+      var xhr = new XMLHttpRequest;
+      xhr.responseType = 'json';
+      xhr.open('GET', DOWNLOAD_URL);
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === SERVER_ANSWER_OK) {
+          onLoad(xhr.response);
+        } else {
+          console.log('error взаимодействия с сервером');
+        }
+      })
+
+      xhr.send();
+    }
+  }
 
 })();
